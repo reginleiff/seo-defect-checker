61d34883ba4f42a0102498df957a2e837162e6cd
Yeo Junkai <reginleiff@gmail.com>
2020-08-13 09:46:20 +0800

    feat: add writer and parser tests

---
+++ test/parser.spec.js
@@ -0,0 +1,21 @@
+const { Readable } = require('stream');
+const { parseInput } = require('../lib/parser');
+
+describe('Parser', () => {
+  describe('Parse Input', () => {
+    test('When provided string, should parse as string input', async () => {
+      const dom = await parseInput('<body></body>');
+      expect(dom('body').length).toBe(1);
+    });
+
+    test('When provided readable stream, should sucessfully parse from stream', async () => {
+      const stream = new Readable.from(['<body></body>']);
+      const dom = await parseInput(stream);
+      expect(dom('body').length).toBe(1);
+    });
+
+    test('When provided other formats, should throw error', async () => {
+      await expect(parseInput(21)).rejects.toThrow('Input is of invalid format');
+    });
+  });
+});
---
+++ test/writer.spec.js
@@ -0,0 +1,44 @@
+const { unlink, readFile } = require('fs');
+const { writeToOutput } = require('../lib/writer');
+const { Writable } = require('stream');
+
+describe('Writer', () => {
+  const testLog = 'This should be written to log';
+  test('When provided console, should print to console log', async () => {
+    const spy = jest.spyOn(console, 'log').mockImplementation();
+    await writeToOutput(testLog, console);
+    expect(console.log).toHaveBeenCalledTimes(1);
+    expect(console.log).toHaveBeenLastCalledWith(testLog);
+    spy.mockRestore();
+  });
+
+  test('When provided filename, should successfully write to file', async () => {
+    const path = __dirname + '/test.txt';
+    console.log(path);
+    await writeToOutput(testLog, path);
+    const writtenData = await new Promise((resolve, reject) => {
+      readFile(path, 'utf-8', (err, data) => {
+        if (err) reject(err);
+        resolve(data);
+      });
+    });
+    expect(writtenData).toEqual(testLog);
+    await new Promise((resolve, reject) => {
+      unlink(path, (err) => (err ? reject(err) : resolve(true)));
+    });
+  });
+
+  test('When provided writable stream, should successfully write to stream', async () => {
+    const stream = new Writable();
+    let data = [];
+    stream._write = (chunk) => {
+      data.push(chunk);
+    };
+    stream.on('end', () => {
+      data = data.join();
+    });
+
+    await writeToOutput(testLog, stream);
+    expect(data.toString()).toEqual(testLog);
+  });
+});
