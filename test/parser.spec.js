0741157d7850c350ddf7bb76d4ed25b9f00a188c
Yeo Junkai <reginleiff@gmail.com>
2020-08-13 11:20:31 +0800

    feat: add parse from file tests

--- test/parser.spec.js
+++ test/parser.spec.js
@@ -1,7 +1,16 @@
 const { Readable } = require('stream');
-const { parseInput } = require('../lib/parser');
+const { parseFromFile, parseInput } = require('../lib/parser');
 
 describe('Parser', () => {
+  describe('Parse File', () => {
+    test('When provided valid file, shoudl parse as string input', async () => {
+      const dom = await parseFromFile(__dirname + '/../examples/sample.html');
+      expect(dom('strong').length).toBe(16);
+      expect(dom('a').length).toBe(4);
+      expect(dom('head').length).toBe(1);
+    });
+  });
+
   describe('Parse Input', () => {
     test('When provided string, should parse as string input', async () => {
       const dom = await parseInput('<body></body>');
